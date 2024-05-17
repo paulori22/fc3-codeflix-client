ed83956c568a232db8f446561bc251874b757ec3
paulori22 <paulinhoboneti@yahoo.com.br>
2024-05-17 10:33:48 -0300

    Introduction - Lesson 12 - Header Hook

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,30 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <header className='fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6'>
+    <header
+      className={`
+        ${isScrolled && 'bg-black'} 
+        fixed top-0 z-50 flex w-full items-center justify-between px-4 py-4 transition-all lg:px-10 lg:py-6`}
+    >
       <div className='flex items-center space-x-2 md:space-x-8'>
         <img src='https://rb.gy/ulxxee' alt='Logo' width={120} height={120} />
         <ul className='hidden md:flex md:space-x-4'>
