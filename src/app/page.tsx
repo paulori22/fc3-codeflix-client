adc2dd064de2c6d205c2a7d415fd55bc0c4c1850
paulori22 <paulinhoboneti@yahoo.com.br>
2024-05-19 12:12:37 -0300

    Introduction - Lesson 20 - Movies Row 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,11 +37,17 @@ export default function Home() {
         </div>
 
         <div className='flex-col space-y-4'>
-          <div className='flex space-x-4 overflow-x-scroll'>
+          <div className='flex'>
+            <h2 className='-ml-2 inline-flex items-center text-2xl font-bold'>
+              Featured
+            </h2>
+          </div>
+          <div className='scrollbar-hide -ml-8 flex space-x-4 overflow-x-scroll p-6'>
             {[1, 2, 3, 4, 5].map((index) => (
               <div
                 key={index}
-                className='group relative h-28 min-w-[200px] transform transition duration-200 ease-in hover:scale-110'
+                className='group relative h-28 min-w-[200px] transform bg-gradient-to-t from-transparent to-black transition duration-200 ease-in hover:z-50 md:h-36 md:min-w-[260px] md:hover:scale-110
+                '
               >
                 <Image
                   src={`/item_${index}.png`}
