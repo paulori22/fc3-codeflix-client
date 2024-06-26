0637c8927083297f97135312aebce7f5eeb4c497
paulori22 <paulinhoboneti@yahoo.com.br>
2024-06-26 17:07:54 -0300

    Fundamental Components - Lesson 15 - Zod Validation 2

--- src/app/auth/login/LoginForm.tsx
+++ src/app/auth/login/LoginForm.tsx
@@ -1,11 +1,41 @@
 'use client';
-import React from 'react';
+import React, { useState } from 'react';
 import { AuthForm } from '@/components/AuthForm';
+import { useRouter } from 'next/router';
+
+type ServerError = {
+  message: string;
+};
 
 export default function LoginForm() {
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const route = useRouter();
+  const [errors, setErrors] = useState<string[]>([]);
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     console.log('submit');
     e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const email = formData.get('email');
+    const password = formData.get('password');
+
+    try {
+      const response = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        route.push('/');
+        return;
+      }
+
+      const payload: ServerError[] = await response.json();
+      setErrors(payload.map((error) => error.message));
+    } catch (error) {
+      console.error(error);
+      setErrors(['n unkown error occurred']);
+    }
   };
   return <AuthForm formType='login' onSubmit={handleSubmit} />;
 }
