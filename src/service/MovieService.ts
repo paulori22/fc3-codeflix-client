66737b15f56ead914f8e84671d759d9effa4e8bd
paulori22 <paulinhoboneti@yahoo.com.br>
2024-06-27 10:41:19 -0300

    API integration and Components - Lesson 5 - Movie Service

---
+++ src/service/MovieService.ts
@@ -0,0 +1,36 @@
+import { RequestOptions, apiRequest } from './ApiRequest';
+import { Movie, Movies } from '@/types/movie';
+
+export const getMovieById = async (id: string): Promise<Movie> => {
+  return apiRequest(`movies/${encodeURIComponent(id)}`);
+};
+
+export const getFeaturesMovie = async (id: string): Promise<Movie> => {
+  return apiRequest(`featured/${id}`);
+};
+
+export const getMoviesByGenre = async (
+  genre: string,
+  options?: RequestOptions
+): Promise<Movies> => {
+  return apiRequest(
+    `movies`,
+    { genre_like: encodeURIComponent(genre) },
+    options
+  );
+};
+
+export const searchMovies = async (
+  title: string = '',
+  genre: string = '',
+  options?: RequestOptions
+): Promise<Movies> => {
+  return apiRequest(
+    `movies`,
+    {
+      title_like: encodeURIComponent(title),
+      genre_like: encodeURIComponent(genre),
+    },
+    options
+  );
+};
