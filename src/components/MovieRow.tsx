4463ec0cba06d0cd4f50ae15af62fa6ce9fd4545
paulori22 <paulinhoboneti@yahoo.com.br>
2024-07-31 11:21:03 -0300

    API integration and Components - Lesson 8 - Movies Card Info 1

--- src/components/MovieRow.tsx
+++ src/components/MovieRow.tsx
@@ -1,5 +1,13 @@
-import { Movies } from '@/types/movie';
+import { Movie, Movies } from '@/types/movie';
+import {
+  ChevronDownIcon,
+  PlayIcon,
+  PlusIcon,
+  SpeakerWaveIcon,
+  UserGroupIcon,
+} from '@heroicons/react/24/outline';
 import Image from 'next/image';
+import Link from 'next/link';
 
 type MovieRowProps = {
   sectionTitle: string;
@@ -7,22 +15,71 @@ type MovieRowProps = {
 };
 
 type MovieCardProps = {
-  index: number;
+  movie: Movie;
 };
 
-const MovieCard = ({ index }: MovieCardProps) => {
+const MovieCard = ({ movie }: MovieCardProps) => {
   return (
     <div
-      key={index}
-      className='group relative h-28 min-w-[200px] transform bg-gradient-to-t from-transparent to-black transition duration-200 ease-in hover:z-50 md:h-36 md:min-w-[260px] md:hover:scale-110
-                '
+      key={movie.id}
+      className='group relative min-h-[12vh] rounded bg-zinc-900 md:min-h-[12vw]'
     >
       <Image
-        src={`/item_${index}.png`}
-        fill={true}
-        alt='Image'
-        className='rounded'
+        src={movie.bannerFileURL}
+        alt={movie.title}
+        width={600}
+        height={400}
+        className='rounded-md object-cover object-top transition'
       />
+      <div
+        className='invisible 
+      absolute top-0 z-10 w-full min-w-[20vw] scale-0 opacity-0 
+      transition 
+      delay-300 duration-200 group-hover:-translate-y-[6vw] group-hover:scale-110 group-hover:opacity-100 sm:visible'
+      >
+        <Image
+          src={movie.bannerFileURL}
+          alt={movie.title}
+          width={600}
+          height={400}
+          className='duration h-[12vw] w-full cursor-pointer rounded-t-md object-cover object-top shadow-xl transition'
+        />
+
+        <div className='absolute z-10 w-full rounded-b-md bg-zinc-800 p-2 shadow-md transition lg:p-4 '>
+          <div className='flex flex-row items-center justify-between gap-2'>
+            <div className='flex flex-row items-center gap-2'>
+              <div className='flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-transparent transition hover:bg-neutral-300 hover:text-black lg:h-10 lg:w-10'>
+                <Link href={`/watch/${movie.id}`}>
+                  <PlayIcon className='w-4 lg:w-6' />
+                </Link>
+              </div>
+              <div className='flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-transparent transition hover:bg-neutral-300 hover:text-black lg:h-10  lg:w-10'>
+                <PlusIcon className='w-4 lg:w-6' />
+              </div>
+            </div>
+            <div className='flex h-6 w-6 cursor-pointer items-center justify-center rounded-full bg-transparent transition hover:bg-neutral-300 hover:text-black lg:h-10 lg:w-10'>
+              <ChevronDownIcon className='w-4 lg:w-6' />
+            </div>
+          </div>
+
+          <div className='mt-4 text-sm font-bold text-white lg:text-lg'>
+            {movie.title}
+          </div>
+
+          <div className='mt-4 flex flex-col gap-2 text-sm'>
+            <div className='flex flex-row items-center gap-2'>
+              <SpeakerWaveIcon className='w-4' /> Double Atmos 5.1 4K
+            </div>
+            <div className='flex flex-row items-center gap-2'>
+              <UserGroupIcon className='w-4' />{' '}
+              {/* <MovieRating rating={movie.rating} /> */}
+            </div>
+          </div>
+          <div className='mt-4 flex flex-row items-center gap-2 text-[8px] text-white lg:text-sm'>
+            <p>{movie.genres.join(' ·  ')}</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
@@ -35,9 +92,9 @@ export default function MovieRow({ sectionTitle, movies }: MovieRowProps) {
           {sectionTitle}
         </h2>
       </div>
-      <div className='-ml-8 flex space-x-4 overflow-x-scroll p-6 scrollbar-hide'>
-        {movies.map((movie, index) => (
-          <MovieCard key={index} index={index} />
+      <div className='grid grid-cols-2 gap-4 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8'>
+        {movies.map((movie) => (
+          <MovieCard key={movie.id} movie={movie} />
         ))}
       </div>
     </div>
